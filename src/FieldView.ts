e3754853b614b967707ce5e5b248171b5d049894
DevilRep <flaringrep@gmail.com>
2021-08-06 17:59:07 +0300

    Implement binding/unbinding event handlers for cells

--- src/FieldView.ts
+++ src/FieldView.ts
@@ -1,6 +1,27 @@
-import {
-    Field
-} from 'fifteen-puzzle-core'
+import {Field} from 'fifteen-puzzle-core'
+import CellView from './CellView'
 
 export default class FieldView extends Field {
+    protected init() {
+        super.init()
+        this.cells.forEach(cell => {
+            const cellView: CellView = <CellView>cell
+            cellView.on('click', async () => await this.move(cell.position))
+        })
+    }
+
+    async move(cellPosition: number): Promise<void> {
+        await super.move(cellPosition)
+        if (this.isWon) {
+            this.won()
+        }
+    }
+
+    protected won() {
+        alert('Congratulations! You won!')
+        this.cells.forEach(cell => {
+            const cellView: CellView = <CellView>cell
+            cellView.off('click')
+        })
+    }
 }
