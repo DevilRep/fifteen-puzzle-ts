819cdc09ea1b8ef27d9d03c3531b4d63f3e18c37
DevilRep <flaringrep@gmail.com>
2021-10-04 22:26:03 +0300

    Added EventBusCell, updated class diagram

---
+++ src/EventBusCell.ts
@@ -0,0 +1,12 @@
+import {AbstractCellDecorator, CellInterface} from 'fifteen-puzzle-core';
+import IEventBus from './interfaces/EventBus'
+
+export default class EventBusCell extends AbstractCellDecorator {
+    public prefixBeforeMove: string = 'move'
+    public prefixAfterMove: string = 'move'
+
+    constructor(cell: CellInterface, eventBus: IEventBus) {
+        super(cell);
+        eventBus = eventBus || eventBus
+    }
+}
---
+++ test/EventBusCell.test.ts
@@ -0,0 +1,67 @@
+import EventBusCell from '../src/EventBusCell'
+
+test('emitting default event before move', async () => {
+    let eventName = ''
+    const cell = new EventBusCell({
+        display: '1',
+        position: 1,
+        move: jest.fn().mockResolvedValue(Promise.resolve()),
+        moveWhileShuffling: jest.fn().mockResolvedValue(Promise.resolve())
+    }, {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: data => eventName || (eventName = data)
+    })
+    await cell.move(1)
+    expect(eventName).toBe('move:start')
+})
+
+test('emitting default event after move', async () => {
+    let eventName = ''
+    const cell = new EventBusCell({
+        display: '1',
+        position: 1,
+        move: jest.fn().mockResolvedValue(Promise.resolve()),
+        moveWhileShuffling: jest.fn().mockResolvedValue(Promise.resolve())
+    }, {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: data => eventName = data
+    })
+    await cell.move(1)
+    expect(eventName).toBe('move:end')
+})
+
+test('emitting custom event before move', async () => {
+    let eventName = ''
+    const cell = new EventBusCell({
+        display: '1',
+        position: 1,
+        move: jest.fn().mockResolvedValue(Promise.resolve()),
+        moveWhileShuffling: jest.fn().mockResolvedValue(Promise.resolve())
+    }, {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: data => eventName || (eventName = data)
+    })
+    cell.prefixBeforeMove = 'test'
+    await cell.move(1)
+    expect(eventName).toBe('test:start')
+})
+
+test('emitting custom event after move', async () => {
+    let eventName = ''
+    const cell = new EventBusCell({
+        display: '1',
+        position: 1,
+        move: jest.fn().mockResolvedValue(Promise.resolve()),
+        moveWhileShuffling: jest.fn().mockResolvedValue(Promise.resolve())
+    }, {
+        on: jest.fn(),
+        off: jest.fn(),
+        emit: data => eventName = data
+    })
+    cell.prefixAfterMove = 'test'
+    await cell.move(1)
+    expect(eventName).toBe('test:end')
+})
