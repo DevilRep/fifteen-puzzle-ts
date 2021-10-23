ad923c18916e3f06949f4d44b10a2d47605833d9
DevilRep <flaringrep@gmail.com>
2021-10-23 22:38:02 +0300

    Updated tests for FieldView

--- src/FieldView.ts
+++ src/FieldView.ts
@@ -1,9 +1,14 @@
 import {Field, CellInterface as Cell} from 'fifteen-puzzle-core'
 import {Modal} from 'materialize-css'
+import {AbstractFactoryInterface} from 'fifteen-puzzle-core'
+import DocumentInterface from './interfaces/Document'
+import EventBusInterface from './interfaces/EventBus';
 
 export default class FieldView extends Field {
     protected element: Element = <Element>{}
     protected modal: Modal = <Modal>{}
+    protected readonly document: DocumentInterface
+    protected readonly eventBus: EventBusInterface
 
     protected initElements(): void {}
 
@@ -19,4 +24,10 @@ export default class FieldView extends Field {
     protected unbindEventHandlersOnCells(): void {}
 
     protected won(): void {}
+
+    public constructor(factory: AbstractFactoryInterface, document: DocumentInterface, eventBus: EventBusInterface) {
+        super(factory)
+        this.document = document
+        this.eventBus = eventBus
+    }
 }
--- test/FieldView.test.ts
+++ test/FieldView.test.ts
@@ -1,154 +1,136 @@
 import FieldView from '../src/FieldView'
 import CellView from '../src/factories/CellView'
 
-const template = `
-    <div class="field">
-        <div class="cell cell1"></div>
-        <div class="cell cell2"></div>
-        <div class="cell cell3"></div>
-        <div class="cell cell4"></div>
-        <div class="cell cell5"></div>
-        <div class="cell cell6"></div>
-        <div class="cell cell7"></div>
-        <div class="cell cell8"></div>
-        <div class="cell cell9"></div>
-        <div class="cell cell10"></div>
-        <div class="cell cell11"></div>
-        <div class="cell cell12"></div>
-        <div class="cell cell13"></div>
-        <div class="cell cell14"></div>
-        <div class="cell cell15"></div>
-        <div class="cell cell16 freeCell"></div>
-    </div>
-    `
+const document = new Document()
+const fieldElement: Element = document.createElement('div')
+fieldElement.classList.add('field')
 
-test('creating a field: are all cellDecorators on the field?', () => {
-    document.body.innerHTML = template
-    const field: FieldView = new FieldView(new CellView())
-    expect(field.toString()).toBe('1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16')
+test('creating a field: are all cells was created', () => {
+    const field: FieldView = new FieldView(
+        {
+            create: jest.fn().mockResolvedValue({
+                display: '1',
+                position: 1,
+                move: jest.fn(),
+                moveWhileShuffling: jest.fn()
+            })
+        },
+        new Document(),
+        {
+            on: jest.fn(),
+            off: jest.fn(),
+            emit: jest.fn(),
+        }
+    )
+    expect(field.toString()).toBe('1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1')
 })
 
 test('creating a field: was added the class `in-game`?', () => {
-    document.body.innerHTML = template
-    const field: FieldView = new FieldView(new CellView())
-    field.toString()
-    const fieldElement: Element = document.querySelector('.field')!
+    const field: FieldView = new FieldView(
+        {
+            create: jest.fn().mockResolvedValue({
+                display: '1',
+                position: 1,
+                move: jest.fn(),
+                moveWhileShuffling: jest.fn()
+            })
+        },
+        document,
+        {
+            on: jest.fn(),
+            off: jest.fn(),
+            emit: jest.fn(),
+        }
+    )
     expect(fieldElement.classList.contains('in-game')).toBeFalsy()
 })
 
 test('starting a new game: was added the class `in-game`?', async () => {
-    document.body.innerHTML = template
-    const field: FieldView = new FieldView(new CellView())
+    const field: FieldView = new FieldView(
+        {
+            create: jest.fn().mockResolvedValue({
+                display: '1',
+                position: 1,
+                move: jest.fn(),
+                moveWhileShuffling: jest.fn()
+            })
+        },
+        document,
+        {
+            on: jest.fn(),
+            off: jest.fn(),
+            emit: jest.fn(),
+        }
+    )
     await field.newGame()
-    const fieldElement: Element = document.querySelector('.field')!
-    expect(fieldElement.classList.contains('in-game')).toBeTruthy()
+    expect(fieldElement.classList.contains('in-game')).toBeFalsy()
 })
 
-test('moving a cell: was changed the free cell position?', async () => {
-    document.body.innerHTML = template
-    const field: FieldView = new FieldView(new CellView())
+test('starting a new game: was changed the free cell position?', async () => {
+    const mock = jest.fn()
+    const field: FieldView = new FieldView(
+        {
+            create: jest.fn()
+                .mockImplementation((position: number, data: string) => ({
+                    display: data,
+                    position,
+                    move: jest.fn(),
+                    moveWhileShuffling: data === '0' ? mock : jest.fn()
+                }))
+        },
+        document,
+        {
+            on: jest.fn(),
+            off: jest.fn(),
+            emit: jest.fn(),
+        }
+    )
     await field.newGame()
-    const freeCell: Element = document.querySelector('.freeCell')!
-    let freeCellPosition: number = 0
-    freeCell.classList.forEach(className => {
-        switch (className) {
-            case 'cell':
-            case 'freeCell':
-                return
-            default:
-                freeCellPosition = +className.slice(4)
-        }
-    })
-    let selectedCell: number
-    if (freeCellPosition <= 4) { // can move cell up
-        selectedCell = freeCellPosition + 4
-    } else { // move down
-        selectedCell = freeCellPosition - 4 // find cell under the free one
-    }
-    const selectedCellElement: Element = document.querySelector(`.cell${selectedCell}`)!
-    selectedCellElement.dispatchEvent(new Event('click'))
-    await new Promise((resolve: Function) => setTimeout(resolve, 1100))
-    freeCell.classList.forEach(className => {
-        switch (className) {
-            case 'cell':
-            case 'freeCell':
-                return
-            default:
-                freeCellPosition = +className.slice(4)
-        }
-    })
-    expect(freeCellPosition).toBe(selectedCell)
+    expect(mock).toBeCalled()
 })
 
-test('moving a cell: was changed the selected cell position?', async () => {
-    document.body.innerHTML = template
-    const field: FieldView = new FieldView(new CellView())
+test('starting a new game: was changed the other cell positions?', async () => {
+    const mock = jest.fn()
+    const field: FieldView = new FieldView(
+        {
+            create: jest.fn()
+                .mockImplementation((position: number, data: string) => ({
+                    display: data,
+                    position,
+                    move: jest.fn(),
+                    moveWhileShuffling: data === '0' ? jest.fn() : mock
+                }))
+        },
+        document,
+        {
+            on: jest.fn(),
+            off: jest.fn(),
+            emit: jest.fn(),
+        }
+    )
     await field.newGame()
-    const freeCell: Element = document.querySelector('.freeCell')!
-    let freeCellPosition: number = 0
-    freeCell.classList.forEach(className => {
-        switch (className) {
-            case 'cell':
-            case 'freeCell':
-                return
-            default:
-                freeCellPosition = +className.slice(4)
-        }
-    })
-    let selectedCell: number
-    if (freeCellPosition <= 4) { // can move cell up
-        selectedCell = freeCellPosition + 4
-    } else { // move down
-        selectedCell = freeCellPosition - 4 // find cell under the free one
-    }
-    const selectedCellElement: Element = document.querySelector(`.cell${selectedCell}`)!
-    selectedCellElement.dispatchEvent(new Event('click'))
-    await new Promise((resolve: Function) => setTimeout(resolve, 1100))
-    freeCell.classList.forEach(className => {
-        switch (className) {
-            case 'cell':
-            case 'freeCell':
-                return
-            default:
-                freeCellPosition = +className.slice(4)
-        }
-    })
-    expect(selectedCell).toBe(freeCellPosition)
+    expect(mock).toBeCalled()
 })
 
 test('moving a cell: was added the class `in-game`?', async () => {
-    document.body.innerHTML = template
-    const field: FieldView = new FieldView(new CellView())
+    fieldElement.classList.remove('in-game')
+    const field: FieldView = new FieldView(
+        {
+            create: jest.fn().mockResolvedValue({
+                display: '1',
+                position: 1,
+                move: jest.fn(),
+                moveWhileShuffling: jest.fn()
+            })
+        },
+        document,
+        {
+            on: jest.fn(),
+            off: jest.fn(),
+            emit: jest.fn(),
+        }
+    )
     await field.newGame()
-    const freeCell: Element = document.querySelector('.freeCell')!
-    let freeCellPosition: number = 0
-    freeCell.classList.forEach(className => {
-        switch (className) {
-            case 'cell':
-            case 'freeCell':
-                return
-            default:
-                freeCellPosition = +className.slice(4)
-        }
-    })
-    let selectedCell: number
-    if (freeCellPosition <= 4) { // can move cell up
-        selectedCell = freeCellPosition + 4
-    } else { // move down
-        selectedCell = freeCellPosition - 4 // find cell under the free one
-    }
-    const selectedCellElement: Element = document.querySelector(`.cell${selectedCell}`)!
-    selectedCellElement.dispatchEvent(new Event('click'))
-    await new Promise((resolve: Function) => setTimeout(resolve, 1100))
-    freeCell.classList.forEach(className => {
-        switch (className) {
-            case 'cell':
-            case 'freeCell':
-                return
-            default:
-                freeCellPosition = +className.slice(4)
-        }
-    })
-    const fieldElement: Element = document.querySelector('.field')!
+    await field.move(15)
     expect(fieldElement.classList.contains('in-game')).toBeTruthy()
 })
